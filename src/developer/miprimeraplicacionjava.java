db9e9bffa858e0e7d8bf6e1f7e522a9fd8233086
SaraiMancia <manciabrego@gmail.com>
2022-03-08 15:27:33 -0600

    datos personales

---
+++ src/developer/miprimeraplicacionjava.java
@@ -0,0 +1,31 @@
+
+package developer;
+
+
+public class miprimeraplicacionjava {
+    public static void main (String []args){
+        
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+        
+        nombreCompleto = "Camila Esperanza Rosales";
+        direccion = "Calle de amargura desvio la tristeza";
+        estadoCivil = " Soltera";
+        edad= 30;
+        gastosDiarios=34.45;
+        genero='f';
+        poseeVehiculo= false;
+        
+        System.out. println("Nombre Completo: "+ nombreCompleto);
+        System.out. println("Direccion: "+ direccion);
+        System.out. println("Estado CIvil: "+estadoCivil);
+System.out. println("Edad: "+edad);
+System.out. println("Gastos Diarioa: "+ gastosDiarios);
+        System.out. println("Genero:"+ genero);
+        System.out. println("Posee Vehiculo. "+ poseeVehiculo);
+    }
+    
+}
