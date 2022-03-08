9c8d0a58a8ff62323bcd6518194a5ecd5d74c505
SaraiMancia <manciabrego@gmail.com>
2022-03-08 14:56:08 -0600

    proyecto 1 java
    sumar numeros

---
+++ src/developer/Variables.java
@@ -0,0 +1,18 @@
+
+package developer;
+
+
+public class Variables {
+
+    public static void main(String[] args) {
+       
+        int suma, a, b;
+        a=12;
+        b=30;
+        
+        suma= a+b;
+        System.out.println("La suma es: "+ suma);
+       
+    }
+    
+}
