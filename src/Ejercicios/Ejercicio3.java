a7381d9120b2225de5b08a25d6126c16e083cdc9
SaraiMancia <manciabrego@gmail.com>
2022-03-18 23:52:58 -0600

    Create Ejercicio3.java

---
+++ src/Ejercicios/Ejercicio3.java
@@ -0,0 +1,24 @@
+
+package Ejercicios;
+
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+       
+    
+    /*-. Un docente universitario gana $8.00 la hora de clase y ha impartido 36 horas en un mes de clases. Debemos de 
+calcular el salario del docente sabiendo que le van a descontar el 10% de la renta. ¿Cuánto ganara el docente al final de
+un mes de clases menos el descuento de la renta?*/
+    
+     double salario, salarioTotal, renta;
+     
+     renta=0.10;
+     
+     salario=8.00*36;
+     salarioTotal= salario-renta;
+     
+        System.out.println("Ganara al final del mes mas el descuento de renta un total de: $"+salarioTotal);
+     
+     
+}
+}
